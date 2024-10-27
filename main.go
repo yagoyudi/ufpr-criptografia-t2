d5c6b27fe36c6c425b6579097666fff583c430cd
Yago Furuta <yagofuruta@protonmail.com>
2024-10-27 09:27:41 -0300

    feat: usando math/big

--- main.go
+++ main.go
@@ -4,97 +4,74 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/big"
 )
 
 // Maior valor de p e q.
 const MaxValuePrimes = 1024
 
 // FindPrimes busca primos (p, q) tal que n=p*q.
-func findPrimes(n int) (int, int, error) {
-	for p := 2; p < MaxValuePrimes; p++ {
-		if n%p == 0 {
-			q := n / p
-			return p, q, nil
+func findPrimes(n *big.Int) (*big.Int, *big.Int, error) {
+	p := big.NewInt(2)
+	limit := big.NewInt(MaxValuePrimes)
+
+	for p.Cmp(limit) < 0 {
+		q := new(big.Int).Div(n, p)             // q = n / p
+		if new(big.Int).Mul(p, q).Cmp(n) == 0 { // Se p * q == n
+			return new(big.Int).Set(p), new(big.Int).Set(q), nil
 		}
+		p.Add(p, big.NewInt(1)) // p++
 	}
-	return 0, 0, fmt.Errorf("erro: findPrimes não encontrou (p, q) válido")
-}
-
-// modInverse calcula o inverso multiplicativo de |a| módulo |m| usando o
-// algoritmo estendido de Euclides.
-func modInverse(a, m int) (int, error) {
-	m0 := m
-	x0, x1 := 0, 1
-
-	if m == 1 {
-		return 0, fmt.Errorf("erro: não existe inverso multiplicativo")
-	}
-
-	for a > 1 {
-		q := a / m
-		m, a = a%m, m
-		x0, x1 = x1-q*x0, x0
-	}
-
-	// Ajusta x1 para ser positivo
-	if x1 < 0 {
-		x1 += m0
-	}
-
-	return x1, nil
+	return nil, nil, fmt.Errorf("erro: findPrimes não encontrou (p, q) válido")
 }
 
 // CalculatePrivateKey retorna {d} com base em {e, n}.
-func calculatePrivateKey(e, n int) (int, error) {
-	if e < 1 {
-		return 0, fmt.Errorf("erro: e < 1")
+func calculatePrivateKey(e, n *big.Int) (*big.Int, error) {
+	one := big.NewInt(1)
+	if e.Cmp(one) <= 0 { // e < 1
+		return nil, fmt.Errorf("erro: e < 1")
 	}
 	p, q, err := findPrimes(n)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
-	phi := (p - 1) * (q - 1)
-	if e >= phi {
-		return 0, fmt.Errorf("erro: e >= phi(n)")
+	phi := new(big.Int).Mul(new(big.Int).Sub(p, one), new(big.Int).Sub(q, one)) // phi(n) = (p - 1) * (q - 1)
+	if e.Cmp(phi) >= 0 {                                                        // e >= phi(n)
+		return nil, fmt.Errorf("erro: e >= phi(n)")
 	}
-	d, err := modInverse(e, phi)
-	if err != nil {
-		return 0, err
+	d := new(big.Int).ModInverse(e, phi) // d = e^-1 (mod phi(n))
+	if d == nil {
+		return nil, fmt.Errorf("erro: não existe inverso multiplicativo")
 	}
 	return d, nil
 }
 
 // Decrypt descriptografa o texto cifrado c usando a chave privada {d, n}.
-func decrypt(c, d, n int) int {
-	plaintext := 1
-	base := c % n
-
-	for d > 0 {
-		if d%2 == 1 {
-			plaintext = (plaintext * base) % n
-		}
-		base = (base * base) % n
-		d /= 2
-	}
-
+func decrypt(c, d, n *big.Int) *big.Int {
+	plaintext := new(big.Int).Exp(c, d, n) // c^d mod n
 	return plaintext
 }
 
 func main() {
-	e := flag.Int("e", 0, "Valor de e")
-	n := flag.Int("n", 0, "Valor de n")
-	c := flag.Int("c", 0, "Valor de c")
+	e := flag.String("e", "", "Valor de e")
+	n := flag.String("n", "", "Valor de n")
+	c := flag.String("c", "", "Valor de c")
 	flag.Parse()
-	if *e == 0 || *n == 0 || *c == 0 {
+	if *e == "" || *n == "" || *c == "" {
 		fmt.Println("Uso: go run main.go -e <valor> -n <valor>")
 		return
 	}
 
-	d, err := calculatePrivateKey(*e, *n)
+	// Converte de string para bit.Int
+	eInt, _ := new(big.Int).SetString(*e, 10)
+	nInt, _ := new(big.Int).SetString(*n, 10)
+	cInt, _ := new(big.Int).SetString(*c, 10)
+
+	d, err := calculatePrivateKey(eInt, nInt)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	plaintext := decrypt(*c, d, *n)
-	fmt.Printf("%d\n", plaintext)
+	plaintext := decrypt(cInt, d, nInt)
+	fmt.Printf("%s\n", plaintext.String())
 }
--- main_test.go
+++ main_test.go
@@ -1,41 +1,33 @@
 package main
 
 import (
+	"math/big"
 	"testing"
 )
 
 func TestFindPrimes(t *testing.T) {
-	n := 187
+	n := big.NewInt(187)
 	p, q, err := findPrimes(n)
 	assertErrorNil(t, err)
-	assertEqual(t, p, 11)
-	assertEqual(t, q, 17)
-}
-
-func TestModInverse(t *testing.T) {
-	a := 3
-	m := 11
-	expected := 4
-	got, err := modInverse(a, m)
-	assertErrorNil(t, err)
-	assertEqual(t, got, expected)
+	assertEqual(t, p, big.NewInt(11))
+	assertEqual(t, q, big.NewInt(17))
 }
 
 func TestCalculatePrivateKey(t *testing.T) {
-	e := 7
-	n := 187
-	want := 23
+	e := big.NewInt(7)
+	n := big.NewInt(187)
+	want := big.NewInt(23)
 	got, err := calculatePrivateKey(e, n)
 	assertErrorNil(t, err)
 	assertEqual(t, got, want)
 }
 
 func TestDecrypt(t *testing.T) {
-	e := 23
-	n := 187
-	c := 11
+	e := big.NewInt(23)
+	n := big.NewInt(187)
+	c := big.NewInt(11)
 	got := decrypt(c, e, n)
-	want := 88
+	want := big.NewInt(88)
 	assertEqual(t, got, want)
 }
 
@@ -46,9 +38,9 @@ func assertErrorNil(t *testing.T, actual error) {
 	}
 }
 
-func assertEqual[T comparable](t *testing.T, actual, expected T) {
+func assertEqual(t *testing.T, actual, expected *big.Int) {
 	t.Helper()
-	if actual != expected {
-		t.Errorf("got %v; want %v", actual, expected)
+	if actual.Cmp(expected) != 0 {
+		t.Errorf("got %v; want %v", actual.String(), expected.String())
 	}
 }
